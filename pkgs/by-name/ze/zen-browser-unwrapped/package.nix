0c96560fc9c83b8f7df81e0c75aaa305687eabc9
Kamil Monicz <kamil@monicz.dev>
2025-09-29 21:09:14 +0200

    zen

--- pkgs/by-name/ze/zen-browser-unwrapped/package.nix
+++ pkgs/by-name/ze/zen-browser-unwrapped/package.nix
@@ -21,6 +21,7 @@ let
   #   attrPath = "zen-browser-unwrapped";
   # };
 in
+(
 (buildMozillaMach {
   pname = "zen-browser";
   inherit (sources) version packageVersion;
@@ -34,14 +35,10 @@ in
   src = fetchurl {
     inherit (sources) hash;
     #url = "https://github.com/zen-browser/desktop/releases/download/${sources.version}/zen.source.tar.zst";
-    url = "https://productionresultssa5.blob.core.windows.net/actions-results/302591ca-fd2b-4ac7-a0f1-69870e081be4/workflow-job-run-7e947fc8-4a71-5736-9864-23974f301f37/artifacts/5dcad2b616229407ec7f33888601409dd74602a8f8c599130e9fbb953b8638fe.zip?rscd=attachment%3B+filename%3D%22zen.source.tar.zst.zip%22&se=2025-09-29T10%3A25%3A25Z&sig=%2ByMvONAJ5hMtGgaFQjNv3dGWJLlW2VaPjHMzBtzmyw8%3D&ske=2025-09-29T19%3A55%3A41Z&skoid=ca7593d4-ee42-46cd-af88-8b886a2f84eb&sks=b&skt=2025-09-29T07%3A55%3A41Z&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skv=2025-11-05&sp=r&spr=https&sr=b&st=2025-09-29T10%3A15%3A20Z&sv=2025-11-05";
+      url = "https://productionresultssa5.blob.core.windows.net/actions-results/302591ca-fd2b-4ac7-a0f1-69870e081be4/workflow-job-run-7e947fc8-4a71-5736-9864-23974f301f37/artifacts/5dcad2b616229407ec7f33888601409dd74602a8f8c599130e9fbb953b8638fe.zip?rscd=attachment%3B+filename%3D%22zen.source.tar.zst.zip%22&se=2025-09-29T14%3A21%3A51Z&sig=nVmi8piZ4xfzySTFqQTj0%2B061leOHFureLdY7bYnnf4%3D&ske=2025-09-30T02%3A10%3A32Z&skoid=ca7593d4-ee42-46cd-af88-8b886a2f84eb&sks=b&skt=2025-09-29T14%3A10%3A32Z&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skv=2025-11-05&sp=r&spr=https&sr=b&st=2025-09-29T14%3A11%3A46Z&sv=2025-11-05";
   };
 
   unpackPhase = "unzip $src && tar -I zstd -xf zen.source.tar.zst && rm zen.source.tar.zst";
-
-  extraPatches = [
-    ./fix-profiling-workspaces.patch
-  ];
 
   extraNativeBuildInputs = [ zstd ];
 
@@ -49,10 +46,7 @@ in
     description = "Firefox fork focused on productivity, privacy, and clean design";
     homepage = "https://zen-browser.app";
     changelog = "https://github.com/zen-browser/desktop/releases/tag/${sources.version}";
-    license = with lib.licenses; [
-      mpl20
-      mit
-    ];
+      license = lib.licenses.mpl20;
     platforms = lib.platforms.unix;
     mainProgram = "zen";
     maxSilent = 14400;
@@ -64,7 +58,17 @@ in
   };
 
   #updateScript = updateScriptDrv;
-}).overrideAttrs
+  }).override
+  {
+    # disable PGO support as the profilingPhase is broken.
+    # reading of the sources indicated that Zen Browser does not support it.
+    pgoSupport = false;
+
+    # crashreporter is not available in Zen Browser.
+    # https://github.com/zen-browser/desktop/blob/2abba4d2668111f44565e4d6d480b4900a77e644/configs/common/mozconfig#L96
+    crashreporterSupport = false;
+  }
+).overrideAttrs
   (old: {
     # version = sources.version;
     # __intentionallyOverridingVersion = true;
