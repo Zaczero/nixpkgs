3ff7eda24a69a9b6ff814ddb0cd363608a2240ea
Kamil Monicz <kamil@monicz.dev>
2025-09-29 15:21:15 +0200

    zen

--- pkgs/by-name/ze/zen-browser-unwrapped/package.nix
+++ pkgs/by-name/ze/zen-browser-unwrapped/package.nix
@@ -34,10 +34,14 @@ in
   src = fetchurl {
     inherit (sources) hash;
     #url = "https://github.com/zen-browser/desktop/releases/download/${sources.version}/zen.source.tar.zst";
-    url = "https://productionresultssa15.blob.core.windows.net/actions-results/4fa0e244-2fca-462c-bb3d-a1c5d6eebd1f/workflow-job-run-a8c9ebc3-7f02-56fe-b41f-a414c20cd398/artifacts/5dcad2b616229407ec7f33888601409dd74602a8f8c599130e9fbb953b8638fe.zip?rscd=attachment%3B+filename%3D%22zen.source.tar.zst.zip%22&se=2025-09-28T20%3A53%3A14Z&sig=mkM4o%2BhH0A%2Fpr%2FD%2BXfPi0iAv8xw41RnxDB2zLYvezqU%3D&ske=2025-09-29T06%3A50%3A51Z&skoid=ca7593d4-ee42-46cd-af88-8b886a2f84eb&sks=b&skt=2025-09-28T18%3A50%3A51Z&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skv=2025-11-05&sp=r&spr=https&sr=b&st=2025-09-28T20%3A43%3A09Z&sv=2025-11-05";
+    url = "https://productionresultssa5.blob.core.windows.net/actions-results/302591ca-fd2b-4ac7-a0f1-69870e081be4/workflow-job-run-7e947fc8-4a71-5736-9864-23974f301f37/artifacts/5dcad2b616229407ec7f33888601409dd74602a8f8c599130e9fbb953b8638fe.zip?rscd=attachment%3B+filename%3D%22zen.source.tar.zst.zip%22&se=2025-09-29T10%3A25%3A25Z&sig=%2ByMvONAJ5hMtGgaFQjNv3dGWJLlW2VaPjHMzBtzmyw8%3D&ske=2025-09-29T19%3A55%3A41Z&skoid=ca7593d4-ee42-46cd-af88-8b886a2f84eb&sks=b&skt=2025-09-29T07%3A55%3A41Z&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skv=2025-11-05&sp=r&spr=https&sr=b&st=2025-09-29T10%3A15%3A20Z&sv=2025-11-05";
   };
 
   unpackPhase = "unzip $src && tar -I zstd -xf zen.source.tar.zst && rm zen.source.tar.zst";
+
+  extraPatches = [
+    ./fix-profiling-workspaces.patch
+  ];
 
   extraNativeBuildInputs = [ zstd ];
 
